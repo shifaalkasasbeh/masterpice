fd53dad86b00edf3ac4c8588370bc368307f086e
shifaalkasasbeh <100359607+shifaalkasasbeh@users.noreply.github.com>
2022-08-13 12:32:17 +0300

    main

--- Admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (isset($_POST['logout'])) {
-    session_destroy();
-    unset($_SESSION['username']);
-}
-header("Location:login.php");
--- include/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=ecommerce' , 'root' , '');
-$GLOBALS['db']= $db;
-} catch (PDOException $e) {
-    echo "somthing wrong please ask the developer about it ";
-}
-
-?>
